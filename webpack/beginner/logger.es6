622db0d15093711a3371920fd842ec19501a1457
Evandro Cavalcante Santos <evandrosantos@mbp-de-evandro.home>
2016-05-12 21:28:12 -0300

    Studying webpack to apply on web projects

---
+++ webpack/beginner/logger.es6
@@ -0,0 +1,8 @@
+let checkName = (firstName, lastName) => {
+if(firstName !== 'nader' || lastName !== 'dabit') {
+console.log( 'You aren\'t Nader Dabit' );
+} else {
+console.log( 'You are Nader Dabit' );
+}
+}
+checkName( 'Nader' , 'Jackson' );
---
+++ webpack/beginner/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	entry: ['./global.js','./app.js'],
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		preloaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'jshint-loader'
+			}
+		],
+		loaders: [
+			{
+				test: /\.es6$/,
+				exclude: 'node_modules',
+				loader: 'babel-loader',
+				query: { 
+					cacheDirectory: true,
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.es6']
+	}
+}
